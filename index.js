d094a43bd1370a6b322ea1c53e71a27fe52aa100
Shubham <shubhamjoshi281@gmail.com>
2024-03-27 13:01:41 +0530

    file update

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ app.get('/', (req, res) => {
   })
 })
 
-
 app.get('/login', (req, res) => {
   res.json({
     message: "this is login"
   })
 })
-
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Running at ${process.env.PORT} Port`)
